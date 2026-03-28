978dd053f7d0d2f276bceea9179bd80fe4a03bbe
imnider <velezzambranoneider@gmail.com>
2026-03-28 11:27:37 -0500

    feat: insertar valores predeterminados a las tablas (database)

---
+++ database/dml.sql
@@ -0,0 +1,41 @@
+USE PokedexClone;
+GO
+
+-- DEFAULT VALUES
+INSERT INTO [MoveCategory]
+	(DisplayName)
+VALUES
+	('Physical'),
+	('Special'),
+	('Status');
+GO
+
+INSERT INTO [Type]
+	(DisplayName)
+VALUES
+	('Normal'),
+    ('Fire'),
+    ('Water'),
+    ('Grass'),
+    ('Electric'),
+    ('Ice'),
+    ('Fighting'),
+    ('Poison'),
+    ('Ground'),
+    ('Flying'),
+    ('Psychic'),
+    ('Bug'),
+    ('Rock'),
+    ('Ghost'),
+    ('Dragon'),
+    ('Steel'),
+    ('Dark'),
+    ('Fairy');
+GO
+
+INSERT INTO MachineType
+	(DisplayName)
+VALUES
+	('Technical Machine'),
+	('Hidden Machine')
+GO
