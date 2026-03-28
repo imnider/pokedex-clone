a1d409ed46471eecdfcd230e56e3413957130073
imnider <velezzambranoneider@gmail.com>
2026-03-28 10:57:57 -0500

    feat: agregar tablas intermedias (database)

--- database/ddl.sql
+++ database/ddl.sql
@@ -10,7 +10,7 @@ CREATE TABLE Type(
 	DisplayName VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Abilty(
+CREATE TABLE Ability(
 	AbilityID UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
 	DisplayName VARCHAR(30) NOT NULL,
 	Effect VARCHAR(255) NOT NULL,
@@ -52,3 +52,55 @@ CREATE TABLE Pokemon(
 );
 
 -- Tablas intermedias (N:M)
+CREATE TABLE PokemonMove(
+	PokemonID INT NOT NULL REFERENCES Pokemon(PokemonID),
+	MoveID UNIQUEIDENTIFIER NOT NULL REFERENCES Move(MoveID),
+	MachineTypeID INT REFERENCES MachineType(MachineTypeID),
+	RequiredLevel INT,
+	RequiresMachine BIT NOT NULL DEFAULT 0,
+	CONSTRAINT PK_PokemonMove_Pokemon_Move
+		PRIMARY KEY (PokemonID, MoveID),
+	CONSTRAINT CK_PokemonMove_MachineType
+		CHECK ((RequiresMachine = 0) OR (RequiresMachine = 1 AND MachineTypeID IS NOT NULL))
+);
+
+CREATE TABLE PokemonType(
+	PokemonID INT NOT NULL REFERENCES Pokemon(PokemonID),
+	TypeID INT NOT NULL REFERENCES Type(TypeID),
+	Slot INT NOT NULL,
+	CONSTRAINT PK_PokemonType_Pokemon_Slot
+		PRIMARY KEY (PokemonID, Slot),
+	CONSTRAINT UK_PokemonType_Pokemon_Type
+		UNIQUE (PokemonID, TypeID),
+	CONSTRAINT CK_PokemonType_Slot
+		CHECK (Slot IN (1,2))
+);
+
+CREATE TABLE PokemonAbility(
+	PokemonID INT NOT NULL REFERENCES Pokemon(PokemonID),
+	AbilityID UNIQUEIDENTIFIER NOT NULL REFERENCES Ability(AbilityID),
+	IsHidden BIT NOT NULL,
+	Slot INT NOT NULL,
+	CONSTRAINT PK_PokemonAbility_Pokemon_Slot
+		PRIMARY KEY (PokemonID, Slot),
+	CONSTRAINT UK_PokemonAbility_Pokemon_Ability
+		UNIQUE (PokemonID, AbilityID),
+	CONSTRAINT CK_PokemonAbility_Slot
+		CHECK (Slot IN (1,3))
+--	BACKEND:
+--		Validar que un Pokémon no pueda tener más de una habilidad oculta
+--		Validar que cuando un Pokémon tiene más de una habilidad, una de ellas debe ser oculta
+);
+
+CREATE TABLE PokemonEvolution(
+	FromPokemonID INT NOT NULL REFERENCES Pokemon(PokemonID),
+	ToPokemonID INT NOT NULL REFERENCES Pokemon(PokemonID),
+	MinLevel INT NULL,
+	IsTrade BIT DEFAULT 0,
+	RequiredItem VARCHAR(50),
+	ConditionDescription VARCHAR(255),
+	CONSTRAINT PK_PokemonEvolution_From_To
+		PRIMARY KEY (FromPokemonID, ToPokemonID),
+--	ACLARACIÓN:
+--		Normalmente RequieredItem sería una Tabla Item, pero por limitaciones de la Pokedex no se agregará dicha tabla
+);
