0eacf4bfadaae5e3e31fe16cb1bb02cb46db7126
imnider <velezzambranoneider@gmail.com>
2026-04-05 20:58:47 -0500

    feat: terminar interfaz del repositorio de Move

--- backend/with-database/PokedexClone/PokedexClone.Domain/Interfaces/Repositories/IMoveRepository.cs
+++ backend/with-database/PokedexClone/PokedexClone.Domain/Interfaces/Repositories/IMoveRepository.cs
@@ -5,6 +5,9 @@ namespace PokedexClone.Domain.Interfaces.Repositories
     public interface IMoveRepository
     {
         public Task<Move> Create(Move move);
-
+        public Task<Move> Get(Guid id);
+        public IQueryable<Move> Queryable();
+        public Task<bool> IfExist(Guid id);
+        public Task<Move> Update(Move move);
     }
 }
